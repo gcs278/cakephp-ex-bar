569ec99ac746dfcd144db6dc5c2752cb5f7470dd
Grant Spence <gspence@redhat.com>
2022-10-03 11:42:55 -0700

    Added demo header bar and bar in the index page for identification

--- webroot/index.php
+++ webroot/index.php
@@ -33,6 +33,8 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use App\Application;
 use Cake\Http\Server;
 
+header('demo: bar');
+
 // Bind your application to the server.
 $server = new Server(new Application(dirname(__DIR__) . '/config'));
 
